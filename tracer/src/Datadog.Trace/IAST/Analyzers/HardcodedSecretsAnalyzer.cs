4a23bfbbe815b14cb5c233ecaf93e7d83d49a671
Daniel Romano <108014683+daniel-romano-DD@users.noreply.github.com>
2023-08-02 23:54:53 +0200

    Alternate method implementation

--- tracer/src/Datadog.Trace/IAST/Analyzers/HardcodedSecretsAnalyzer.cs
+++ tracer/src/Datadog.Trace/IAST/Analyzers/HardcodedSecretsAnalyzer.cs
@@ -13,215 +13,269 @@ using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using Datadog.Trace.AppSec;
 using Datadog.Trace.AppSec.Waf;
+using Datadog.Trace.ClrProfiler;
 using Datadog.Trace.Configuration;
+using Datadog.Trace.Debugger.PInvoke;
 using Datadog.Trace.Logging;
 using Datadog.Trace.Vendors.dnlib.DotNet.Resources;
 
-namespace Datadog.Trace.Iast.Analyzers
+namespace Datadog.Trace.Iast.Analyzers;
+
+internal class HardcodedSecretsAnalyzer
 {
-    internal class HardcodedSecretsAnalyzer
+    private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor<HardcodedSecretsAnalyzer>();
+    private static HardcodedSecretsAnalyzer? _instance = null;
+    private static string[] _excludedAssemblies = new[]
     {
-        private static readonly IDatadogLogger Log = DatadogLogging.GetLoggerFor<HardcodedSecretsAnalyzer>();
-        private static HardcodedSecretsAnalyzer? _instance = null;
-        private static string[] _excludedAssemblies = new[]
-        {
-            "System*",
-            "Datadog.*",
-            "Kudu*",
-            "Microsoft*",
-            "MSBuild",
-            "dotnet",
-            "netstandard",
-            "AspNet.*",
-            "msvcm90*",
-            "Mono.*",
-            "NuGet.*",
-            "PCRE.*",
-            "Antlr*",
-            "Azure.Messaging.ServiceBus*",
-            "PostSharp",
-            "SMDiagnostics",
-            "testhost",
-            "WebGrease",
-            "YamlDotNet",
-            "EnvSettings*",
-            "EntityFramework*",
-            "linq2db*",
-            "Newtonsoft.Json*",
-            "log4net*",
-            "Autofac*",
-            "StackExchange*",
-            "BundleTransformer*",
-            "LibSassHost*",
-            "ClearScript*",
-            "NewRelic*",
-            "AppDynamics*",
-            "NProfiler*",
-            "KTJdotnetTls*",
-            "KTJUniDC*",
-            "Dynatrace*",
-            "oneagent*",
-            "CommandLine",
-            "Moq",
-            "Castle.Core",
-            "MiniProfiler*",
-            "MySql*",
-            "Serilog*",
-            "ServiceStack*",
-            "mscorlib",
-            "Xunit.*",
-            "xunit.*",
-            "FluentAssertions",
-            "NUnit3.TestAdapter",
-            "nunit.*",
-            "vstest.console",
-            "testhost.*",
-            "Oracle.ManagedDataAccess",
-        };
+        "System*",
+        "Datadog.*",
+        "Kudu*",
+        "Microsoft*",
+        "MSBuild",
+        "dotnet",
+        "netstandard",
+        "AspNet.*",
+        "msvcm90*",
+        "Mono.*",
+        "NuGet.*",
+        "PCRE.*",
+        "Antlr*",
+        "Azure.Messaging.ServiceBus*",
+        "PostSharp",
+        "SMDiagnostics",
+        "testhost",
+        "WebGrease",
+        "YamlDotNet",
+        "EnvSettings*",
+        "EntityFramework*",
+        "linq2db*",
+        "Newtonsoft.Json*",
+        "log4net*",
+        "Autofac*",
+        "StackExchange*",
+        "BundleTransformer*",
+        "LibSassHost*",
+        "ClearScript*",
+        "NewRelic*",
+        "AppDynamics*",
+        "NProfiler*",
+        "KTJdotnetTls*",
+        "KTJUniDC*",
+        "Dynatrace*",
+        "oneagent*",
+        "CommandLine",
+        "Moq",
+        "Castle.Core",
+        "MiniProfiler*",
+        "MySql*",
+        "Serilog*",
+        "ServiceStack*",
+        "mscorlib",
+        "Xunit.*",
+        "xunit.*",
+        "FluentAssertions",
+        "NUnit3.TestAdapter",
+        "nunit.*",
+        "vstest.console",
+        "testhost.*",
+        "Oracle.ManagedDataAccess",
+    };
 
-        private static List<Regex>? _excludedAssembliesRegexes = null;
+    private static List<Regex>? _excludedAssembliesRegexes = null;
+    private static bool _started = false;
+    private static ManualResetEventSlim _waitEvent = new ManualResetEventSlim(false);
 
-        public HardcodedSecretsAnalyzer()
-        {
-            LifetimeManager.Instance.AddShutdownTask(RunShutdown);
+    public HardcodedSecretsAnalyzer()
+    {
+        LifetimeManager.Instance.AddShutdownTask(RunShutdown);
 
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            AppDomain.CurrentDomain.AssemblyLoad += CurrentDomain_AssemblyLoad;
-            Task.Run(() => ProcessAssemblies(assemblies));
-        }
+        // var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        // AppDomain.CurrentDomain.AssemblyLoad += CurrentDomain_AssemblyLoad;
+        // Task.Run(() => ProcessAssemblies(assemblies));
 
-        private void CurrentDomain_AssemblyLoad(object? sender, AssemblyLoadEventArgs args)
-        {
-            Task.Run(() => ProcessAssemblies(args.LoadedAssembly));
-        }
+        _started = true;
+        Task.Run(() => PoolingThread());
+    }
 
-        internal void ProcessAssemblies(params Assembly[] assemblies)
+    private static void PoolingThread()
+    {
+        while (_started)
         {
             var waf = Security.Instance.WafInitResult?.Waf;
             if (waf != null)
             {
-                foreach (var assembly in assemblies)
+                var timeout = Security.Instance.Settings.WafTimeoutMicroSeconds;
+                var userStrings = new UserStringInterop[100];
+                int userStringLen = NativeMethods.GetUserStrings(userStrings.Length, userStrings);
+                if (userStringLen > 0)
                 {
-                    if (IsExcluded(assembly)) { continue; }
-                    var secrets = ProcessAssembly(waf, Security.Instance.Settings.WafTimeoutMicroSeconds, assembly);
-                    if (secrets.Count > 0)
-                    {
-                        List<Vulnerability> vulnerabilities = new List<Vulnerability>();
-                        var location = assembly.Location;
-                        foreach (var secret in secrets)
-                        {
-                            vulnerabilities.Add(new Vulnerability(
-                                VulnerabilityTypeName.HardcodedSecret,
-                                (VulnerabilityTypeName.HardcodedSecret + ":" + location + ":" + secret).GetStaticHashCode(),
-                                new Location(location),
-                                new Evidence(secret),
-                                IntegrationId.HardcodedSecret));
-                        }
+                    List<Vulnerability> vulnerabilities = new List<Vulnerability>();
 
+                    for (int x = 0; x < userStringLen; x++)
+                    {
+                        using var context = waf.CreateContext();
+                        if (context != null)
+                        {
+                            var value = Marshal.PtrToStringUni(userStrings[x].Value);
+                            if (CheckSecret(context!, value!, timeout))
+                            {
+                                var location = Marshal.PtrToStringUni(userStrings[x].Location);
+                                vulnerabilities.Add(new Vulnerability(
+                                    VulnerabilityTypeName.HardcodedSecret,
+                                    (VulnerabilityTypeName.HardcodedSecret + ":" + location! + ":" + value!).GetStaticHashCode(),
+                                    new Location(location!),
+                                    new Evidence(value!),
+                                    IntegrationId.HardcodedSecret));
+                            }
+                        }
+                    }
+
+                    if (vulnerabilities.Count > 0)
+                    {
                         IastModule.OnHardcodedSecret(vulnerabilities);
                     }
                 }
             }
+
+            _waitEvent.Wait(10_000);
         }
+    }
 
-        private static List<string> ProcessAssembly(IWaf waf, ulong timeout, Assembly assembly)
+    private void CurrentDomain_AssemblyLoad(object? sender, AssemblyLoadEventArgs args)
+    {
+        Task.Run(() => ProcessAssemblies(args.LoadedAssembly));
+    }
+
+    internal void ProcessAssemblies(params Assembly[] assemblies)
+    {
+        var waf = Security.Instance.WafInitResult?.Waf;
+        if (waf != null)
         {
-            List<string> res = new List<string>();
-            try
+            foreach (var assembly in assemblies)
             {
-                using var fs = new FileStream(assembly.Location, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-                using var peReader = new PEReader(fs);
-                MetadataReader mr = peReader.GetMetadataReader();
-
-                UserStringHandle userStringHandle = mr.GetNextHandle(new UserStringHandle());
-                while (!userStringHandle.IsNil)
+                if (IsExcluded(assembly)) { continue; }
+                var secrets = ProcessAssembly(waf, Security.Instance.Settings.WafTimeoutMicroSeconds, assembly);
+                if (secrets.Count > 0)
                 {
-                    var userString = mr.GetUserString(userStringHandle);
-                    using var context = waf.CreateContext();
-                    if (context != null)
+                    List<Vulnerability> vulnerabilities = new List<Vulnerability>();
+                    var location = assembly.Location;
+                    foreach (var secret in secrets)
                     {
-                        if (CheckSecret(context, userString, timeout))
-                        {
-                            res.Add(userString);
-                        }
+                        vulnerabilities.Add(new Vulnerability(
+                            VulnerabilityTypeName.HardcodedSecret,
+                            (VulnerabilityTypeName.HardcodedSecret + ":" + location + ":" + secret).GetStaticHashCode(),
+                            new Location(location),
+                            new Evidence(secret),
+                            IntegrationId.HardcodedSecret));
                     }
 
-                    userStringHandle = mr.GetNextHandle(userStringHandle);
+                    IastModule.OnHardcodedSecret(vulnerabilities);
                 }
             }
-            catch (Exception err)
-            {
-                Log.Warning(err, "Error processing assembly {0}", assembly);
-            }
-
-            return res;
         }
+    }
 
-        internal static bool CheckSecret(IContext context, string secret, ulong timeout)
+    private static List<string> ProcessAssembly(IWaf waf, ulong timeout, Assembly assembly)
+    {
+        List<string> res = new List<string>();
+        try
         {
-            var args = new Dictionary<string, object> { { "hardcoded.secret", secret } };
+            using var fs = new FileStream(assembly.Location, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using var peReader = new PEReader(fs);
+            MetadataReader mr = peReader.GetMetadataReader();
 
-            var result = context.Run(args, timeout);
-            return (result != null && result.ReturnCode == ReturnCode.Match);
+            UserStringHandle userStringHandle = mr.GetNextHandle(new UserStringHandle());
+            while (!userStringHandle.IsNil)
+            {
+                var userString = mr.GetUserString(userStringHandle);
+                using var context = waf.CreateContext();
+                if (context != null)
+                {
+                    if (CheckSecret(context, userString, timeout))
+                    {
+                        res.Add(userString);
+                    }
+                }
+
+                userStringHandle = mr.GetNextHandle(userStringHandle);
+            }
+        }
+        catch (Exception err)
+        {
+            Log.Warning(err, "Error processing assembly {0}", assembly);
         }
 
-        internal static void Initialize()
+        return res;
+    }
+
+    internal static bool CheckSecret(IContext context, string secret, ulong timeout)
+    {
+        var args = new Dictionary<string, object> { { "hardcoded.secret", secret } };
+
+        var result = context.Run(args, timeout);
+        return (result != null && result.ReturnCode == ReturnCode.Match);
+    }
+
+    internal static void Initialize()
+    {
+        lock (Log)
+        {
+            if (_instance == null)
+            {
+                _instance = new HardcodedSecretsAnalyzer();
+            }
+        }
+    }
+
+    internal static bool IsExcluded(Assembly assembly)
+    {
+        if (_excludedAssembliesRegexes == null)
         {
             lock (Log)
             {
-                if (_instance == null)
+                if (_excludedAssembliesRegexes == null)
                 {
-                    _instance = new HardcodedSecretsAnalyzer();
-                }
-            }
-        }
+                    _excludedAssembliesRegexes = new List<Regex>();
 
-        internal static bool IsExcluded(Assembly assembly)
-        {
-            if (_excludedAssembliesRegexes == null)
-            {
-                lock (Log)
-                {
-                    if (_excludedAssembliesRegexes == null)
+                    // Construct exclussion regexes
+                    foreach (var txt in _excludedAssemblies)
                     {
-                        _excludedAssembliesRegexes = new List<Regex>();
-
-                        // Construct exclussion regexes
-                        foreach (var txt in _excludedAssemblies)
-                        {
-                            var regex = new Regex(txt.Replace(".", "/.").Replace("*", ".*"), RegexOptions.IgnoreCase | RegexOptions.Compiled);
-                            _excludedAssembliesRegexes.Add(regex);
-                        }
+                        var regex = new Regex(txt.Replace(".", "/.").Replace("*", ".*"), RegexOptions.IgnoreCase | RegexOptions.Compiled);
+                        _excludedAssembliesRegexes.Add(regex);
                     }
                 }
             }
-
-            if (assembly.IsDynamic) { return true; }
-
-            foreach (var regex in _excludedAssembliesRegexes)
-            {
-                var name = assembly.GetName().Name;
-                if (name == null || regex.IsMatch(name))
-                {
-                    return true;
-                }
-            }
-
-            return false;
         }
 
-        private void RunShutdown()
+        if (assembly.IsDynamic) { return true; }
+
+        foreach (var regex in _excludedAssembliesRegexes)
         {
-            try
+            var name = assembly.GetName().Name;
+            if (name == null || regex.IsMatch(name))
             {
-                AppDomain.CurrentDomain.AssemblyLoad -= CurrentDomain_AssemblyLoad;
+                return true;
             }
-            catch { }
         }
+
+        return false;
+    }
+
+    private void RunShutdown()
+    {
+        try
+        {
+            _started = false;
+            _waitEvent.Set();
+            AppDomain.CurrentDomain.AssemblyLoad -= CurrentDomain_AssemblyLoad;
+        }
+        catch { }
     }
 }
---
+++ tracer/src/Datadog.Tracer.Native/iast/method_analyzer.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "../../../../shared/src/native-src/pal.h"
+#include <atomic>
+#include <map>
+#include <set>
+using namespace shared;
+
+namespace iast
+{
+    class MethodInfo;
+
+    class MethodAnalyzer
+    {
+    public:
+        virtual bool ProcessMethod(MethodInfo* method) = 0;
+    };
+} // namespace iast
